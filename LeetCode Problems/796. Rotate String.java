ba26d7f8c506c6d5e0c23d33e08b1678d03630c5
Lokesh Chintalapudi <136214157+Lokeshloki4uhe@users.noreply.github.com>
2024-06-26 21:41:26 +0530

    Add files via upload

---
+++ LeetCode Problems/796. Rotate String.java
@@ -0,0 +1,7 @@
+//LEETCODE 796. Rotate String
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        return s.length() == goal.length() && (s+s).contains(goal);
+    }
+}
