07eec9a37c78acef66399c28d1f6597966a8999e
Lokesh Chintalapudi <136214157+Lokeshloki4uhe@users.noreply.github.com>
2024-07-11 22:04:15 +0530

    Add files via upload

---
+++ LeetCode Problems/81. Search in Rotated Sorted Array II.java
@@ -0,0 +1,10 @@
+//LEETCODE 81. Search in Rotated Sorted Array II
+
+class Solution {
+    public boolean search(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target) return true;
+        }
+        return false;
+    }
+}
