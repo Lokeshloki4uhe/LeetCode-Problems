6dab3d7a2af583460bac231b2cda3c799fff0788
Lokesh Chintalapudi <136214157+Lokeshloki4uhe@users.noreply.github.com>
2024-09-05 22:34:04 +0530

    Add files via upload

---
+++ LeetCode Problems/136. Single Number.java
@@ -0,0 +1,11 @@
+//LEETCODE 136. Single Number
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for(int i=0;i<nums.length;i++){
+            xor = xor^nums[i];
+        }
+        return xor;
+    }
+}
