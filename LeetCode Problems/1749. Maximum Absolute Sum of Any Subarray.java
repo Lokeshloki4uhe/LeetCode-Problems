629805e4178dc358da965dc261dbe0773a0eda98
Lokesh Chintalapudi <136214157+Lokeshloki4uhe@users.noreply.github.com>
2025-02-26 21:24:40 +0530

    Add files via upload

---
+++ LeetCode Problems/1749. Maximum Absolute Sum of Any Subarray.java
@@ -0,0 +1,23 @@
+//LEETCODE 1749. Maximum Absolute Sum of Any Subarray
+
+
+class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        int sum = 0, minSum = 0, maxSum = 0;
+        for (int num : nums) {
+            sum += num;
+            if (sum > maxSum) maxSum = sum;
+            if (sum < minSum) minSum = sum;
+        }
+        return Math.abs(maxSum - minSum);
+    }
+}
+
+
+
+
+
+
+
+
+
