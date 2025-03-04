bcf581db51d86e9bf4d6f5b6bc5926e813f4114d
Lokesh Chintalapudi <136214157+Lokeshloki4uhe@users.noreply.github.com>
2025-03-04 22:02:38 +0530

    Add files via upload

---
+++ LeetCode Problems/1780. Check if Number is a Sum of Powers of Three.java
@@ -0,0 +1,23 @@
+//LEETCODE 1780. Check if Number is a Sum of Powers of Three 
+
+
+class Solution {
+    public boolean checkPowersOfThree(int n) {
+        while (n > 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
