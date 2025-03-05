8aacefd34a9074df1cc4b9d2a42bf6747bb96d4e
Lokesh Chintalapudi <136214157+Lokeshloki4uhe@users.noreply.github.com>
2025-03-05 21:16:11 +0530

    Add files via upload

---
+++ LeetCode Problems/2579. Count Total Number of Colored Cells.java
@@ -0,0 +1,17 @@
+//LEETCODE 2579. Count Total Number of Colored Cells 
+
+
+class Solution {
+    public long coloredCells(int n) {
+        return 1 + 4L * n * (n - 1) / 2; 
+    }
+}
+
+
+
+
+
+
+
+
+
