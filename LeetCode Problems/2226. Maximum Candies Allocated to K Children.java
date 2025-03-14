3ed745cc8496b2d81928494f656c65a35b9cf8b2
Lokesh Chintalapudi <136214157+Lokeshloki4uhe@users.noreply.github.com>
2025-03-14 20:38:23 +0530

    Add files via upload

---
+++ LeetCode Problems/2226. Maximum Candies Allocated to K Children.java
@@ -0,0 +1,32 @@
+//LEETCODE 2226. Maximum Candies Allocated to K Children
+
+
+class Solution {
+    public int maximumCandies(int[] candies, long k) {
+        int left = 1, right = 10_000_000;
+        int result = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long childrenCount = 0;
+            for (int candy : candies) {
+                childrenCount += candy / mid;
+            }
+            if (childrenCount >= k) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+}
+
+
+
+
+
+
+
+
+
