c0b15f8488eea44591bcf540f8945f101cf05e7d
Lokesh Chintalapudi <136214157+Lokeshloki4uhe@users.noreply.github.com>
2025-03-17 18:33:20 +0530

    Add files via upload

---
+++ LeetCode Problems/2206. Divide Array Into Equal Pairs.java
@@ -0,0 +1,21 @@
+//LEETCODE 2206. Divide Array Into Equal Pairs
+
+
+class Solution {
+    public boolean divideArray(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i += 2) {
+            if (nums[i] != nums[i + 1]) return false;
+        }
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
