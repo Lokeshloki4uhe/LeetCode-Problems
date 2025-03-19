dcc40632ab817bf53871f52567582c86b03c0326
Lokesh Chintalapudi <136214157+Lokeshloki4uhe@users.noreply.github.com>
2025-03-19 22:22:10 +0530

    Add files via upload

---
+++ LeetCode Problems/3191. Minimum Operations to Make Binary Array Elements Equal to One I.java
@@ -0,0 +1,28 @@
+//LEETCODE 3191. Minimum Operations to Make Binary Array Elements Equal to One I
+
+
+class Solution {
+    public int minOperations(int[] nums) {
+        int n = nums.length, k = 0;
+        for (int i = 0; i <= n - 3; i++) {
+            if (nums[i] == 0) {
+                nums[i] ^= 1;
+                nums[i + 1] ^= 1;
+                nums[i + 2] ^= 1;
+                k++;
+            }
+        }
+        for (int num : nums) {
+            if (num == 0) return -1;
+        }
+        return k;
+    }
+}
+
+
+
+
+
+
+
+
